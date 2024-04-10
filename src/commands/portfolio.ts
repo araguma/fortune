b8c62059c463f268886aff150785bc2f9997c55c
araguma <itetbitb@gmail.com>
2024-04-09 23:30:28 -0400

    feat: add new stats

--- src/commands/portfolio.ts
+++ src/commands/portfolio.ts
@@ -21,74 +21,88 @@ discord.addCommand({
             interaction.options.getUser('user')?.id ?? interaction.user.id,
         )
 
-        const shares = await database.getAllShares(interaction.user.id)
         const snapshots = await alpaca.snapshots(
-            shares.map((share) => share.symbol),
+            Array.from(client.portfolio.keys()),
         )
 
-        const value = shares.reduce((acc, share) => {
-            const snapshot = snapshots[share.symbol]
-            if (!snapshot) throw new Error('Failed to get snapshot')
-            return acc + snapshot.latestTrade.p * share.quantity
-        }, 0)
-        const delta = shares.reduce((acc, share) => {
-            const snapshot = snapshots[share.symbol]
-            if (!snapshot) throw new Error('Failed to get snapshot')
-            return (
-                acc +
-                (snapshot.latestTrade.p - snapshot.dailyBar.o) * share.quantity
-            )
-        }, 0)
-        const description = shares
-            .map((share) => {
-                const snapshot = snapshots[share.symbol]
+        const { value, delta } = Array.from(client.portfolio.entries()).reduce(
+            (acc, [symbol, share]) => {
+                const snapshot = snapshots[symbol]
                 if (!snapshot) throw new Error('Failed to get snapshot')
-                const symbol = share.symbol
-                const quantity = share.quantity
                 const quote = snapshot.latestTrade.p
-                const total = quote * quantity
+                return {
+                    value: acc.value + quote * (share.quantity ?? 0),
+                    delta:
+                        acc.delta +
+                        (quote - snapshot.dailyBar.o) * (share.quantity ?? 0),
+                }
+            },
+            { value: 0, delta: 0 },
+        )
+        const total = value + client.balance
+        const profit = total - client.seed
+        const description = Array.from(client.portfolio.entries())
+            .map(([symbol, share]) => {
+                const snapshot = snapshots[symbol]
+                if (!snapshot) throw new Error('Failed to get snapshot')
+                const quote = snapshot.latestTrade.p
+                const total = quote * (share.quantity ?? 0)
                 const percent =
                     ((quote - snapshot.dailyBar.o) / snapshot.dailyBar.o) * 100
-                const sign = percent >= 0 ? '▴' : '▾'
-                return `${sign} **${symbol}** ${quantity} ⋅ $${quote} ▸ $${total.toFixed(2)} (${percent.toFixed(2)}%)`
+                const sign = percent > 0 ? '▴' : percent < 0 ? '▾' : '•'
+                return `${sign} **${symbol}** ${share.quantity ?? 0} ⋅ $${quote} ▸ $${total.toFixed(2)} (${percent.toFixed(2)}%)`
             })
             .join('\n')
-
-        await interaction.reply({
-            embeds: [
+        const embed = {
+            color: delta > 0 ? 0x2ecc71 : delta < 0 ? 0xe74c3c : 0x3498db,
+            author: {
+                name: '---',
+            },
+            title: 'Portfolio',
+            description: description || '> *No shares found*',
+            fields: [
                 {
-                    color: delta >= 0 ? 0x2ecc71 : 0xe74c3c,
-                    author: {
-                        name: '---',
-                    },
-                    title: 'Portfolio',
-                    description: description || '> *No shares found*',
-                    fields: [
-                        {
-                            name: 'Value',
-                            value: `$${value.toFixed(2)} (${value === 0 ? '0.00' : ((delta / value) * 100).toFixed(2)}%)`,
-                            inline: true,
-                        },
-                        {
-                            name: 'Balance',
-                            value: `$${client.balance.toFixed(2)}`,
-                            inline: true,
-                        },
-                        {
-                            name: 'Total',
-                            value: `$${(value + client.balance).toFixed(2)} (${((delta / (value + client.balance)) * 100).toFixed(2)}%)`,
-                            inline: true,
-                        },
-                    ],
-                    image: {
-                        url: 'attachment://divider.png',
-                    },
-                    footer: {
-                        text: client._id.toString().toUpperCase(),
-                    },
-                    timestamp: new Date().toISOString(),
+                    name: 'Value',
+                    value: `$${value.toFixed(2)} (${(value ? (delta / value) * 100 : 0).toFixed(2)}%)`,
+                    inline: true,
+                },
+                {
+                    name: 'Balance',
+                    value: `$${client.balance.toFixed(2)}`,
+                    inline: true,
+                },
+                {
+                    name: 'Total',
+                    value: `$${total.toFixed(2)} (${(total ? (delta / total) * 100 : 0).toFixed(2)}%)`,
+                    inline: true,
+                },
+                {
+                    name: 'Delta',
+                    value: `$${delta.toFixed(2)}`,
+                    inline: true,
+                },
+                {
+                    name: 'Seed',
+                    value: `$${client.seed.toFixed(2)}`,
+                    inline: true,
+                },
+                {
+                    name: 'Profit',
+                    value: `$${profit.toFixed(2)} (${(profit ? (delta / profit) * 100 : 0).toFixed(2)}%)`,
+                    inline: true,
                 },
             ],
+            image: {
+                url: 'attachment://divider.png',
+            },
+            footer: {
+                text: client._id.toString().toUpperCase(),
+            },
+            timestamp: new Date().toISOString(),
+        }
+
+        await interaction.reply({
+            embeds: [embed],
             files: [
                 {
                     attachment: divider(),
