651e73af4eefaebe868f53e75b930e832e541db1
araguma <itetbitb@gmail.com>
2024-04-18 16:19:58 -0400

    feat: include pool per option

--- src/libs/reply/prediction.ts
+++ src/libs/reply/prediction.ts
@@ -28,7 +28,11 @@ export class PredictionReply {
                 ...this.options.map((option, index) => {
                     const percentage =
                         pool === 0 ? 0 : (this.pool[index] ?? 0) / pool
-                    return `> ${index + 1}. ${option}\n> ${'I'.repeat(percentage * 64)}[${format.percentage(percentage)}]\n`
+                    return [
+                        `> ${index + 1}) ${option}`,
+                        `> ${'I'.repeat(percentage * 64)}[${format.percentage(percentage)}]`,
+                        `> ${format.currency(this.pool[index] ?? 0)}\n`,
+                    ].join('\n')
                 }),
                 this.status === 'opened'
                     ? 'Use **/predict** to place your bets'
