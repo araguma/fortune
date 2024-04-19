54db322167bb73690eaf08e405511bc89090379e
araguma <itetbitb@gmail.com>
2024-04-19 03:33:59 -0400

    refactor: read PORT from environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 
+if (!process.env['PORT']) throw new Error('PORT not set')
+
 const app = express()
 
 app.get('/ping', function (_, response) {
     response.send('pong')
 })
 
-app.listen(3000)
+app.listen(parseInt(process.env['PORT']), () => {
+    console.log(`Web server started on port ${process.env['PORT']}`)
+})
