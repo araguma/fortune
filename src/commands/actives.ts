4e7c3e5b2090a0956bc9551aebc7295fb2b26049
araguma <itetbitb@gmail.com>
2024-05-22 22:17:33 -0400

    feat: add view select and update button to embed

--- src/commands/actives.ts
+++ src/commands/actives.ts
@@ -1,6 +1,9 @@
 import { Group } from '@/enums'
+import UserError from '@/errors/user'
 import Command from '@/libs/command'
 import { getEnvironmentVariable } from '@/libs/env'
+import Tag from '@/libs/tag'
+import { generateViewReply } from '@/libs/view'
 import alpaca from '@/services/alpaca'
 import discord from '@/services/discord'
 import { ActivesReply } from '@/views/actives'
@@ -35,10 +38,48 @@ command.setChatInputCommandHandler(async (interaction) => {
     const actives = await alpaca.getActives(FIELD_LINE_LIMIT, by)
 
     const reply = new ActivesReply({
+        userId: interaction.user.id,
         by,
         actives,
     })
     await interaction.reply(reply)
+})
+
+command.setButtonHandler(async (interaction) => {
+    const tag = new Tag(interaction.customId)
+
+    switch (tag.getAction(true)) {
+        case 'update': {
+            const userId = tag.getData('userId', true)
+            const by = tag.getData('by', true) as 'volume' | 'trades'
+            const actives = await alpaca.getActives(FIELD_LINE_LIMIT, by)
+
+            const reply = new ActivesReply({
+                userId,
+                by,
+                actives,
+            })
+            await interaction.update(reply)
+            break
+        }
+    }
+})
+
+command.setStringSelectMenuHandler(async (interaction) => {
+    const tag = new Tag(interaction.customId)
+
+    switch (tag.getAction(true)) {
+        case 'view': {
+            const userId = tag.getData('userId', true)
+            const symbol = interaction.values[0]
+
+            if (!symbol) UserError.missingSymbol()
+
+            const reply = await generateViewReply(symbol, '1D', userId)
+            await interaction.reply(reply)
+            break
+        }
+    }
 })
 
 discord.addCommand(command)
--- src/views/actives.ts
+++ src/views/actives.ts
@@ -1,7 +1,15 @@
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    StringSelectMenuBuilder,
+} from 'discord.js'
+
 import { Color } from '@/enums'
 import divider from '@/libs/canvas/divider'
 import format from '@/libs/format'
 import Reply from '@/libs/reply'
+import Tag from '@/libs/tag'
 import { AlpacaActive } from '@/services/alpaca'
 
 export class ActivesReply extends Reply<ActivesReplyData> {
@@ -10,7 +18,47 @@ export class ActivesReply extends Reply<ActivesReplyData> {
         this.update(data)
     }
 
-    public override update({ by, actives }: ActivesReplyData) {
+    public override update({ userId, by, actives }: ActivesReplyData) {
+        const viewSelect = new StringSelectMenuBuilder()
+            .setCustomId(
+                new Tag()
+                    .setCommand('actives')
+                    .setAction('view')
+                    .setData('userId', userId)
+                    .toCustomId(),
+            )
+            .setPlaceholder('View symbol')
+            .addOptions(
+                actives.map((active) => ({
+                    label: active.symbol,
+                    value: active.symbol,
+                })),
+            )
+        const updateButton = new ButtonBuilder()
+            .setCustomId(
+                new Tag()
+                    .setCommand('actives')
+                    .setAction('update')
+                    .setData('by', by)
+                    .setData('userId', userId)
+                    .toCustomId(),
+            )
+            .setLabel('Update')
+            .setStyle(ButtonStyle.Secondary)
+        const row1 =
+            new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+                viewSelect,
+            )
+        const row2 = new ActionRowBuilder<ButtonBuilder>().addComponents(
+            updateButton,
+        )
+
+        const rows: ActionRowBuilder<
+            StringSelectMenuBuilder | ButtonBuilder
+        >[] = []
+        if (actives.length > 0) rows.push(row1)
+        rows.push(row2)
+
         this.setColor(Color.Blue)
         this.setAuthor({ name: '---' })
         this.setTitle(`Active Stocks by ${format.capitalize(by)}`)
@@ -32,10 +80,12 @@ export class ActivesReply extends Reply<ActivesReplyData> {
             },
         )
         this.setCanvas(divider())
+        this.setComponents(rows)
     }
 }
 
 export type ActivesReplyData = {
+    userId: string
     by: 'volume' | 'trades'
     actives: AlpacaActive[]
 }
