6e5b5b306f93adeec9c4fcc43ce85bb6a528bd32
araguma <itetbitb@gmail.com>
2024-05-22 22:29:05 -0400

    feat: add view select and update button to embed

--- src/commands/movers.ts
+++ src/commands/movers.ts
@@ -1,6 +1,9 @@
 import { Group } from '@/enums'
+import UserError from '@/errors/user'
 import Command from '@/libs/command'
 import { getEnvironmentVariable } from '@/libs/env'
+import Tag from '@/libs/tag'
+import { generateViewReply } from '@/libs/view'
 import alpaca from '@/services/alpaca'
 import discord from '@/services/discord'
 import { MoversReply } from '@/views/movers'
@@ -35,10 +38,50 @@ command.setChatInputCommandHandler(async (interaction) => {
     const movers = await alpaca.getMovers(FIELD_LINE_LIMIT, 'stocks')
 
     const reply = new MoversReply({
+        userId: interaction.user.id,
         by,
         movers: movers[by],
     })
     await interaction.reply(reply)
+})
+
+command.setButtonHandler(async (interaction) => {
+    const tag = new Tag(interaction.customId)
+
+    switch (tag.getAction(true)) {
+        case 'update': {
+            const userId = tag.getData('userId', true)
+            const by = tag.getData('by', true).toLowerCase() as
+                | 'gainers'
+                | 'losers'
+            const movers = await alpaca.getMovers(FIELD_LINE_LIMIT, 'stocks')
+
+            const reply = new MoversReply({
+                userId,
+                by,
+                movers: movers[by],
+            })
+            await interaction.update(reply)
+            break
+        }
+    }
+})
+
+command.setStringSelectMenuHandler(async (interaction) => {
+    const tag = new Tag(interaction.customId)
+
+    switch (tag.getAction(true)) {
+        case 'view': {
+            const userId = tag.getData('userId', true)
+            const symbol = interaction.values[0]
+
+            if (!symbol) UserError.missingSymbol()
+
+            const reply = await generateViewReply(symbol, '1D', userId)
+            await interaction.reply(reply)
+            break
+        }
+    }
 })
 
 discord.addCommand(command)
--- src/views/movers.ts
+++ src/views/movers.ts
@@ -1,7 +1,15 @@
+import {
+    StringSelectMenuBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ActionRowBuilder,
+} from 'discord.js'
+
 import { Color } from '@/enums'
 import divider from '@/libs/canvas/divider'
 import format from '@/libs/format'
 import Reply from '@/libs/reply'
+import Tag from '@/libs/tag'
 import { AlpacaMover } from '@/services/alpaca'
 
 const variant = {
@@ -21,8 +29,48 @@ export class MoversReply extends Reply<MoversReplyData> {
         this.update(data)
     }
 
-    public override update({ by, movers }: MoversReplyData) {
+    public override update({ userId, by, movers }: MoversReplyData) {
         const { color, sign } = variant[by]
+
+        const viewSelect = new StringSelectMenuBuilder()
+            .setCustomId(
+                new Tag()
+                    .setCommand('movers')
+                    .setAction('view')
+                    .setData('userId', userId)
+                    .toCustomId(),
+            )
+            .setPlaceholder('View symbol')
+            .addOptions(
+                movers.map((mover) => ({
+                    label: mover.symbol,
+                    value: mover.symbol,
+                })),
+            )
+        const updateButton = new ButtonBuilder()
+            .setCustomId(
+                new Tag()
+                    .setCommand('movers')
+                    .setAction('update')
+                    .setData('by', by)
+                    .setData('userId', userId)
+                    .toCustomId(),
+            )
+            .setLabel('Update')
+            .setStyle(ButtonStyle.Secondary)
+        const row1 =
+            new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+                viewSelect,
+            )
+        const row2 = new ActionRowBuilder<ButtonBuilder>().addComponents(
+            updateButton,
+        )
+
+        const rows: ActionRowBuilder<
+            StringSelectMenuBuilder | ButtonBuilder
+        >[] = []
+        if (movers.length > 0) rows.push(row1)
+        rows.push(row2)
 
         this.setColor(color)
         this.setAuthor({ name: '---' })
@@ -41,10 +89,12 @@ export class MoversReply extends Reply<MoversReplyData> {
         )
         this.setCanvas(divider())
         this.setTimestamp(new Date())
+        this.setComponents(rows)
     }
 }
 
 export type MoversReplyData = {
+    userId: string
     by: 'gainers' | 'losers'
     movers: AlpacaMover[]
 }
